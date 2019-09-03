64f78e8b0a0037c01488b8fe5808f6a2d340ccb7
Alex1 <2473134393@qq.com>
2019-09-03 17:27:19 +0800

    add DecisionTreeClassifier

--- src/main/scala/com/littlely/spark/machinelearning/Classifications.scala
+++ src/main/scala/com/littlely/spark/machinelearning/Classifications.scala
@@ -1,20 +1,29 @@
 package com.littlely.spark.machinelearning
 
-import org.apache.spark.ml.classification.{LogisticRegression, LogisticRegressionModel}
+import org.apache.spark.SparkConf
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.ml.classification.{DecisionTreeClassificationModel, DecisionTreeClassifier, LogisticRegression, LogisticRegressionModel}
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer, StringIndexerModel, VectorIndexer, VectorIndexerModel}
 import org.apache.spark.ml.linalg.{Vector, Vectors}
 import org.apache.spark.ml.param.ParamMap
-import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
 object Classifications {
 
   def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("Classification")
+    val spark: SparkSession = SparkSession.builder().config(conf).getOrCreate()
 
+    val classifications: Classifications = new Classifications(spark)
+    classifications.getDecisionTreeClassifer()
   }
 }
 
 
 class Classifications(spark : SparkSession){
 
+  // LogisticRegression model
   def getLogisticRegression(): Unit ={
 
     // 创建训练集
@@ -59,5 +68,40 @@ class Classifications(spark : SparkSession){
     model_2.transform(test).select("features", "label", "myProbability", "prediction").collect()
       .foreach{case Row(features : Vector, label : Double, prob : Vector, prediction : Double) =>
         println(s"($features, $label) -> prob=$prob, prediction=$prediction")}
+  }
+
+  def getDecisionTreeClassifer(): Unit ={
+    // load data from hdfs
+    val data: DataFrame = spark.read.format("libsvm").load("hdfs://centos03:9000/spark-data/mllib/sample_libsvm_data.txt")
+
+    val labelIndexer: StringIndexerModel = new StringIndexer().setInputCol("label").setOutputCol("indexedLabel").fit(data)
+    val featureIndexer: VectorIndexerModel = new VectorIndexer().setInputCol("features").setOutputCol("indexedFeatures").setMaxCategories(4).fit(data)
+    labelIndexer.transform(data).show(3)
+    featureIndexer.transform(data).show(3)
+
+    val Array(trainingData, testData): Array[Dataset[Row]] = data.randomSplit(Array(0.7, 0.3))
+
+    val dt: DecisionTreeClassifier = new DecisionTreeClassifier().setLabelCol("indexedLabel").setFeaturesCol("indexedFeatures")
+
+    // Convert indexed labels back to original labels
+    val labelConverter: IndexToString = new IndexToString().setInputCol("prediction").setOutputCol("predictedLabel").setLabels(labelIndexer.labels)
+
+    val pipeline: Pipeline = new Pipeline().setStages(Array(labelIndexer, featureIndexer, dt, labelConverter))
+
+    //train model
+    val model: PipelineModel = pipeline.fit(trainingData)
+    val predictions: DataFrame = model.transform(testData)
+
+    predictions.show(5) //select("predictedLabel", "label", "features").show(5)
+
+    // calc test error
+    val evaluator: MulticlassClassificationEvaluator = new MulticlassClassificationEvaluator().setLabelCol("indexedLabel").setPredictionCol("prediction").setMetricName("accuracy")
+    val acc: Double = evaluator.evaluate(predictions)
+    println("Test Error = " + (1.0 - acc))
+
+    // model.stages(2) means the second stage of model: dt
+    val treeModel: DecisionTreeClassificationModel = model.stages(2).asInstanceOf[DecisionTreeClassificationModel]
+    println("Learned classification tree model: " + treeModel.toDebugString)
+
   }
 }
